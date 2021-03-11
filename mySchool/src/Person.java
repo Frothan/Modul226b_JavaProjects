3698f6849233c8cecdd40f6878c931f2b819c602
Bismillah Frothan <45491920+BismilalhFrothan@users.noreply.github.com>
2021-03-11 10:43:34 +0100

    Add files via upload

---
+++ mySchool/src/Person.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+public class Person {
+	String name;
+	String firstName;
+	String dateOfBirth;
+	String email;
+	
+	public Person(String name, String firstName){
+		this.name = name;
+		this.firstName = firstName;
+	}
+
+	public void display() {
+		System.out.println("name " + name + " " + firstName);
+		System.out.println();
+
+	}
+}
